718dc674fdfd47d452c9a47cb12fdc122e08396c
diprima (ISIDE Win10) <diprima@planetek.it>
2017-05-24 17:44:14 +0200

    merge from pc PK

--- Dottorato/Algoritm_streak_detection/CPP/algoritm_streak_detection_cpp/astrometricReduction.sh
+++ Dottorato/Algoritm_streak_detection/CPP/algoritm_streak_detection_cpp/astrometricReduction.sh
@@ -3,6 +3,8 @@ echo "========================================"
 echo "Astrometric reduction"
 echo "Start processing"
 
+#SOLVE_BIN=${SOLVE_BIN}
+
 inParam=$#
 expParam=2
 expParamArcsecpix=4
@@ -41,10 +43,11 @@ then
 	solve-field --dir $resWcsFile --overwrite --cpulimit 30 --no-plots --guess-scale --scale-units arcsecperpix --scale-low $arcsecpixScaleL --scale-high $arcsecpixScaleH $imgName
 else
 	solve-field --dir $resWcsFile --overwrite --cpulimit 30 --no-plots --guess-scale $imgName
-	#/cygdrive/c/cygwin/lib/astrometry/bin/solve-field.exe --dir $resWcsFile --overwrite --cpulimit 30 --no-plots --guess-scale $imgName
+	#/cygdrive/c/cygwin/lib/astrometry/bin/solve-field.exe --dir $resWcsFile --overwrite --cpulimit 30 --no-plots --guess-scale $imgName  
 fi
 
 #solve-field --overwrite --cpulimit 30 --guess-scale --no-plots --scale-units arcsecperpix --scale-low $arcsecpixScaleL --scale-high $arcsecpixScaleH $imgName
+
 
 echo "End processing"
 echo "========================================"
